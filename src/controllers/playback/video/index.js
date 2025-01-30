86583c5fa0c1f60cb119e08686b88cc841bf4ca5
Frost_I7 <87358895+FrostI7Alex@users.noreply.github.com>
2025-01-30 23:50:56 +0700

    feat: implement horizontal scroll seeking

--- src/controllers/playback/video/index.js
+++ src/controllers/playback/video/index.js
@@ -1440,6 +1440,12 @@ export default function (view) {
         if (e.deltaY > 0) {
             playbackManager.volumeDown(currentPlayer);
         }
+        if (e.deltaX < 0) {
+            playbackManager.rewind(currentPlayer);
+        }
+        if (e.deltaX > 0) {
+            playbackManager.fastForward(currentPlayer);
+        }
     }
 
     function onWindowMouseDown(e) {
